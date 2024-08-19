5eb14ae4df0c7e241b66e7ee156fea03c6b4604e
Milde, Manuel <Manuel.Milde@de.kaercher.com>
2024-08-19 15:00:41 +0200

    Map users to read_only role, and add variable for enabling audit logs in dashboard

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "applications" {
   description = "Define the permission for each service role you want to create"
-  type        = map(object({
+  type = map(object({
     backend_role_names  = set(string)
     cluster_permissions = set(string)
     index_patterns      = set(string)
@@ -16,4 +16,10 @@ variable "admin_users" {
 variable "users" {
   type        = set(string)
   description = "Set of your normal users"
+}
+
+variable "enable_audit_logs" {
+  type        = bool
+  description = "Enable audit logs"
+  default     = false
 }
